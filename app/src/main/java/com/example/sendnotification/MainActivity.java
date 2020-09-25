87ae890b65fe78bd7d5bcf4b3a8b669a097f27f9
Utkarsh Gupta <utkarshgupta0808@gmail.com>
2020-09-26 00:01:11 +0530

    Added send notification feature to different apps.

--- app/src/main/java/com/example/sendnotification/MainActivity.java
+++ app/src/main/java/com/example/sendnotification/MainActivity.java
@@ -3,44 +3,115 @@ package com.example.sendnotification;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+    private RequestQueue mRequestQue;
+    private String URL = "https://fcm.googleapis.com/fcm/send";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button=findViewById(R.id.btn_notification);
+        if (getIntent().hasExtra("category")){
+            Intent intent = new Intent(MainActivity.this,RecieveNotificationActivity.class);
+            intent.putExtra("category",getIntent().getStringExtra("category"));
+            intent.putExtra("brandId",getIntent().getStringExtra("brandId"));
+            startActivity(intent);
+        }
+        Button button = findViewById(R.id.btn);
+        mRequestQue = Volley.newRequestQueue(this);
+        FirebaseMessaging.getInstance().subscribeToTopic("news");
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String message="This is an example notification";
-                NotificationCompat.Builder builder= new NotificationCompat.Builder(MainActivity.this)
-                        .setSmallIcon(R.drawable.ic_message)
-                        .setContentTitle("Grocy")
-                        .setContentText(message)
-                        .setAutoCancel(true);
-//                Intent intent=new Intent(MainActivity.this, NotificationActivity.class);
-//                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//                intent.putExtra("message",message);
-//
-//                PendingIntent pendingIntent=PendingIntent.getActivity(MainActivity.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-//                builder.setContentIntent(pendingIntent);
-                NotificationManager notificationManager= (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-                notificationManager.notify(0, builder.build());
-
+            public void onClick(View v) {
+                sendNotification();
             }
         });
+
+    }
+
+
+
+
+    private void sendNotification() {
+
+        JSONObject json = new JSONObject();
+        try {
+            json.put("to","/topics/"+"news");
+            JSONObject notificationObj = new JSONObject();
+            notificationObj.put("title","any title");
+            notificationObj.put("body","any body");
+
+            JSONObject extraData = new JSONObject();
+            extraData.put("brandId","puma");
+            extraData.put("category","Shoes");
+
+
+
+            json.put("notification",notificationObj);
+            json.put("data",extraData);
+
+
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, URL,
+                    json,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+
+                            Log.d("MUR", "onResponse: ");
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("MUR", "onError: "+error.networkResponse);
+                }
+            }
+            ){
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String,String> header = new HashMap<>();
+                    header.put("content-type","application/json");
+                    header.put("authorization","key=AAAA7k2rlxk:APA91bEA2-If8_pQB4-IsfMnbd2UOnl0n6rR3ay6rYP3JBqqSFv4IE8V6o7XC_he4bXt_LVxvhskcOESSqDDQPwSQ4ESuYKSMJKaUKRfvusQCgBxg83JqGeKDb7lKId60dtRKLqh37rK");
+                    return header;
+                }
+            };
+            mRequestQue.add(request);
+        }
+        catch (JSONException e)
+
+        {
+            e.printStackTrace();
+        }
+
+
     }
 }
